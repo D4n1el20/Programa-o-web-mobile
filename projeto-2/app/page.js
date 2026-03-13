5e2e52549a599c2c7c873e56a89419853e605272
D4n1el20 <daniel1405costa@gmail.com>
2026-03-13 12:01:09 +0000

    Acrescentando partes do Curriculo(nao finalizado)

--- projeto-2/app/page.js
+++ projeto-2/app/page.js
@@ -5,60 +5,50 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
+          <h1>Daniel Silva Costa</h1>
+          <p>Sou aluno do curso de Ciência da Computação</p>
+          <h2>Resumo</h2>
+          <p>Estudante procurando estágio na minha área,tenho pouca experiência o ambito profissional,mas já fiz alguns projetos acadêmicos</p>
+          <h2>
+            Experiência profissional
+          </h2>
+          <h4>
+            Jovem Aprendiz Administrativo do Financeiro, ASA Industria
+          </h4>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Criei borderôs
           </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <p>
+            Comparação de dados
+          </p>
+          <p>
+            Uso de tabelas excel
+          </p>
+          <h2>
+            Experiência acadêmica
+          </h2>
+          <h4>
+            Projeto extencionista, Biblioteca Carangueijo Tabaiares
+          </h4>
+          <p>Workshop de Cubo Mágico para crianças que frequentavam a biblioteca. A proposta foi ajudar a desenvolver curiosidade, criatividade e cordenação motora</p>
+          <p>Workshop de Excel para funcionários da biblioteca. A proposta foi auxiliar funcionários com as dificuldades que tinham com o uso de excel</p>
+          <h4>Projeto extensionista, Colégio Liceu Nóbrega de Artes e Ofícios</h4>
+          <p>Foi feita uma APi com o objetivo de auxiliar uma equipe de robótica, Coliceu, que em 2025 estavam com objetivo de fazer algo que auxiliasse os arqueologos.
+            Eles tiveram a ideia de dar um kit de arqueologia e nos pediram um app que ajudasse a mapear sitios arqueologicos.</p>
+            <p>Api feita para funcionar de forma offline com mapa estilo xadrez expansivel.No período de 2 meses,concluímos o projeto.</p>
+          <h4>
+            Jogo da velha, Marcio Augusto Silva Bueno
+          </h4>
+          <p>
+            Trabalho que fiz solicitado pelo professor da cadeira de Programação web e mobile. pode ser acessado clicando aqui(decidir se vai ser link ou botão)
+          </p>
+          <h4>
+            Habilidades
+          </h4>
+          <h4>Educação</h4>
+          <h4>Detalhes de Contato</h4>
         </div>
       </main>
     </div>
